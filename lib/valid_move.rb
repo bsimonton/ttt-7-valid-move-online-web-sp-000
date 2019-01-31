6d0f4981897e1b031f1d73c3c8a76a39e4697a17
Brian Simonton <bsimonton78@gmail.com>
2019-01-31 18:10:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,12 @@
 # code your #valid_move? method here
+
+
+
+
 def valid_move()
 
 
 
 
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
-def position_taken?(board, index)
-  board[index] != " " && board[index] != "" && board[index] != nil
-end
